578f2c197a0854c4469836cb4a9fa62327693062
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-14 20:38:45 +0530

    notification entity updated | notification repository updated.

--- src/main/java/lk/ijse/SE10_NETWORK_BACKEND/repository/NotificationRepository.java
+++ src/main/java/lk/ijse/SE10_NETWORK_BACKEND/repository/NotificationRepository.java
@@ -1,9 +1,15 @@
 package lk.ijse.SE10_NETWORK_BACKEND.repository;
 
 import lk.ijse.SE10_NETWORK_BACKEND.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Query("select n from Notification n WHERE n.user.userId=:userId")
+    Page<Notification> findByStudentId(@Param("userId") Long studentId, Pageable pageable);
 }
