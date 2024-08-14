978804eeeebe62a892d97d68a29910fca4b8332d
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-14 20:40:28 +0530

    notification service layer implemented.

--- src/main/java/lk/ijse/SE10_NETWORK_BACKEND/service/NotificationService.java
+++ src/main/java/lk/ijse/SE10_NETWORK_BACKEND/service/NotificationService.java
@@ -1,7 +1,12 @@
 package lk.ijse.SE10_NETWORK_BACKEND.service;
 
+import lk.ijse.SE10_NETWORK_BACKEND.dto.NotificationDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface NotificationService {
+    NotificationDTO saveNotification(NotificationDTO dto);
+    List<NotificationDTO> getNotificationsByStudentId(Long userId, Integer pageNo, Integer notificationCount);
 }
