227af6c1f1fb8413efca0a18ad9e17673549ce57
Sasmith Manawadu <sasmithgit@gmail.com>
2024-08-14 22:08:26 +0530

    post controller layer updated

--- src/main/java/lk/ijse/SE10_NETWORK_BACKEND/controller/PostController.java
+++ src/main/java/lk/ijse/SE10_NETWORK_BACKEND/controller/PostController.java
@@ -1,46 +1,77 @@
 package lk.ijse.SE10_NETWORK_BACKEND.controller;
 
 import lk.ijse.SE10_NETWORK_BACKEND.dto.PostDTO;
+import lk.ijse.SE10_NETWORK_BACKEND.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
+    @Autowired
+    private PostService postService;
+
     @PostMapping("/save")
     public ResponseEntity<String> savePost(@RequestBody PostDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body("Post saved successfully");
+        PostDTO postDTO = postService.savePost(dto);
+
+        if (postDTO != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Post saved successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Post could not be saved");
+        }
     }
 
     @PutMapping("/update")
     public ResponseEntity<String> updatePost(@RequestBody PostDTO dto) {
-        return ResponseEntity.ok().body("Post updated successfully");
+        PostDTO postDTO = postService.updatePost(dto);
+
+        if (postDTO != null) {
+            return ResponseEntity.status(HttpStatus.OK).body("Post updated successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post could not be updated");
+        }
     }
 
     @PutMapping("/{postId}/status")
     public ResponseEntity<String> updateStatus(
             @PathVariable("postId") Long postId,
             @RequestParam("adminId") Long adminId) {
-        return ResponseEntity.ok().body("Post id: " + postId + " approved by admin id: " + adminId);
+        PostDTO postDTO = postService.updatePostStatus(postId, adminId);
+
+        if (postDTO != null) {
+            return ResponseEntity.status(HttpStatus.OK).body("Post status updated successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post could not be updated");
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PostDTO> getPost(@PathVariable Long id) {
-        PostDTO dto = new PostDTO();
-        dto.setPostId(id);
-        return ResponseEntity.ok().body(dto);
+        PostDTO postById = postService.getPostById(id);
+
+        if (postById != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(postById);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<PostDTO>> getAllPost(
             @RequestParam("pageNo") Integer pageNo,
             @RequestParam("postCount") Integer postCount) {
-        List<PostDTO> dtos = new ArrayList<>();
-        return ResponseEntity.ok().body(dtos);
+        List<PostDTO> allPosts = postService.getAllPosts(pageNo, postCount);
+
+        if (allPosts != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(allPosts);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @GetMapping("/user/{id}")
@@ -48,15 +79,25 @@ public class PostController {
             @PathVariable("id") Long userId,
             @RequestParam("pageNo") Integer pageNo,
             @RequestParam("postCount") Integer postCount) {
-        List<PostDTO> dtos = new ArrayList<>();
-        return ResponseEntity.ok().body(dtos);
+        List<PostDTO> posts = postService.getAllPostsOfStudent(userId, pageNo, postCount);
+
+        if (posts != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(posts);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @GetMapping("/unapproved")
     public ResponseEntity<List<PostDTO>> getUnapprovedPosts(
             @RequestParam("pageNo") Integer pageNo,
             @RequestParam("postCount") Integer postCount) {
-        List<PostDTO> dtos = new ArrayList<>();
-        return ResponseEntity.ok().body(dtos);
+        List<PostDTO> posts = postService.getUnapprovedPosts(pageNo, postCount);
+
+        if (posts != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(posts);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 }
